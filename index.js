1493174a77435e53b6a8aea6afb4db0cbbf96d9f
SethCohen <sethcohen.dev@gmail.com>
2022-08-23 22:32:21 -0400

    feat: updated action

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
+const fetch = require('node-fetch')
+
+async function getContext () {
+    const payload = github.context.payload
+
+    return {
+        body: payload.release.body.length < 1500
+            ? payload.release.body
+            : payload.release.body.substring(0, 1500) + ` ([...](${payload.release.html_url}))`,
+        version: payload.release.tag_name,
+        html_url: payload.release.html_url
+    }
 }
+
+async function run () {
+    try {
+        const webhookUrl = core.getInput('webhook_url')
+
+        if (!webhookUrl) {
+            return core.setFailed('webhook_url not set. Please set it.')
+        }
+
+        const content = await getContext()
+
+        const embedMsg = {
+            color: 3447003,
+            title: `Release ${content.version}`,
+            description: content.body,
+            url: content.html_url
+        }
+
+        const body = { embeds: [embedMsg] }
+
+        const url = `${webhookUrl}?wait=true`
+
+        fetch(url, {
+            method: 'post',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(res => res.json())
+            .then(data => core.info(JSON.stringify(data)))
+            .catch(err => core.info(err))
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+run()
