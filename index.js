3d770807cc43343bde8371ec581e244b5f9591e4
Ryan Hullah <rhullah@ostusa.com>
2023-12-11 10:03:00 -0500

    properly limit embed length
    - add commits to linked description
    - tweak some regex
    - change default description length to the max limit of 4096

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ import fetch from 'node-fetch';
 /**
  * Stylizes a markdown body into an appropriate embed message style.
  *  Remove HTML comments (commonly added by 'Generate release notes' button)
- *  Better URL linking for commong Github links: PRs, Issues, Compare
+ *  Better URL linking for common Github links: PRs, Issues, Compare
  *  Redundant whitespace and newlines removed, keeping at max 2 to provide space between paragraphs
  *  Trim leading/trailing whitespace
  *  If reduce_headings:
  *   H3s converted to bold and underlined
  *   H2s converted to bold
- * @param description
- * @returns {*}
+ * @param {string} description
  */
 const formatDescription = (description) => {
     let edit = description
-        .replace(/<!--.*?-->/g, '')
+        .replace(/<!--.*?-->/gs, '')
         .replace(
             new RegExp(
-                "https://github.com/(.+)/(.+)/(issues|pull|compare)/(\\S+)",
+                "https://github.com/(.+)/(.+)/(issues|pull|commit|compare)/(\\S+)",
                 "g"
             ),
             (match, user, repo, type, id) => {
-                return `[${getUrlPrefix(type) + id}](${match})`;
+                return `[${getTypePrefix(type) + id}](${match})`
             }
         )
         .replace(/\n\s*\n/g, (ws) => {
@@ -34,8 +33,8 @@ const formatDescription = (description) => {
 
     if (core.getBooleanInput('reduce_headings')) {
         edit = edit
-            .replace(/^### (.*?)$/gm, '**__$1__**')
-            .replace(/^## (.*?)$/gm, '**$1**')
+            .replace(/^###\s+(.+)$/gm, '**__$1__**')
+            .replace(/^##\s+(.+)$/gm, '**$1**')
     }
 
     return edit
@@ -43,14 +42,16 @@ const formatDescription = (description) => {
 
 /**
  * Get a prefix to use for Github link display
- * @param {'issues' | 'pull' | 'compare'} type 
+ * @param {'issues' | 'pull' | 'commit' | 'compare'} type 
  */
-function getUrlPrefix (type) {
+function getTypePrefix (type) {
     switch (type) {
         case 'issues':
             return 'Issue #'
         case 'pull':
             return 'PR #'
+        case 'commit':
+            return 'Commit #'
         case 'compare':
             return ''
         default:
@@ -60,44 +61,62 @@ function getUrlPrefix (type) {
 
 /**
  * Gets the max description length if set to a valid number,
- * otherwise the default of 1500
- * @returns {number}
+ * otherwise the default of 4096
  */
 function getMaxDescription () {
     try {
         const max = core.getInput('max_description')
         if (typeof max === 'string' && max.length) {
+            // 4096 is max for Embed Description
             // https://discord.com/developers/docs/resources/channel#embed-object-embed-limits
-            // 4096 is max for Embed Description, minus 500 to allow for elipsis and link
-            return Math.min(parseInt(max, 10), 4096 - 500)
+            return Math.min(parseInt(max, 10), 4096)
         }
     } catch (err) {
         core.warning(`max_description not a valid number: ${err}`)
     }
-    return 1500
+    return 4096
 }
 
 /**
  * Get the context of the action, returns a GitHub Release payload.
- * @returns {Promise<{html_url, body: (*|string), name: string}>}
  */
-async function getContext () {
+function getContext () {
     const payload = github.context.payload;
-    const maxDesc = getMaxDescription()
 
     return {
-        body: payload.release.body.length < maxDesc
-            ? payload.release.body
-            : payload.release.body.substring(0, maxDesc) + ` ([...](${payload.release.html_url}))`,
+        body: payload.release.body,
         name: payload.release.name,
         html_url: payload.release.html_url
     }
 }
 
 /**
+ * 
+ * @param {string} str
+ * @param {number} maxLength
+ * @param {string=} url
+ */
+function limit(str, maxLength, url) {
+    if (str.length <= maxLength)
+        return str
+    let replacement = '…'
+    if (url) {
+        replacement = `([${replacement}](${url}))`
+    }
+    maxLength = maxLength - replacement.length
+    str = str.substring(0, maxLength)
+
+    const lastWhitespace = str.search(/[^\s]*$/)
+    if (lastWhitespace > -1) {
+        str = str.substring(0, lastWhitespace)
+    }
+
+    return str + replacement
+}
+
+/**
  * Handles the action.
  * Get inputs, creates a stylized response webhook, and sends it to the channel.
- * @returns {Promise<void>}
  */
 async function run () {
     const webhookUrl = core.getInput('webhook_url');
@@ -111,21 +130,24 @@ async function run () {
 
     if (!webhookUrl) return core.setFailed('webhook_url not set. Please set it.');
 
-    const {body, html_url, name} = await getContext();
+    const {body, html_url, name} = getContext();
 
     const description = formatDescription(body);
 
     let embedMsg = {
-        title: name,
+        title: limit(name, 256),
         url: html_url,
         color: color,
         description: description,
         footer: {}
     }
 
-    if (footerTitle != '') embedMsg.footer.text = footerTitle;
+    if (footerTitle != '') embedMsg.footer.text = limit(footerTitle, 2048);
     if (footerIconUrl != '') embedMsg.footer.icon_url = footerIconUrl;
     if (footerTimestamp == 'true') embedMsg.timestamp = new Date().toISOString();
+
+    let embedSize = embedMsg.title.length + (embedMsg.footer?.text?.length ?? 0)
+    embedMsg.description = limit(embedMsg.description, Math.min(getMaxDescription(), 6000 - embedSize), embedMsg.url)
 
     let requestBody = {
         embeds: [embedMsg]
